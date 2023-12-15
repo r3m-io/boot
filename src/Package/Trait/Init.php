5f0196b056c78b1f04210849c3fee24445622096
remco van der Velde <remco.pc@outlook.com>
2023-12-15 17:59:13 +0100

    initializing boot - added init trait

--- src/Package/Trait/Init.php
+++ src/Package/Trait/Init.php
@@ -3,19 +3,23 @@ namespace Package\R3m\Io\Boot\Trait;
 
 use R3m\Io\Module\Core;
 use R3m\Io\Module\File;
+
+use R3m\Io\Node\Model\Node;
 trait Init {
+
 
     public function installation (){
         $object = $this->object();
         $url_package = $object->config('project.dir.vendor') . 'r3m_io/boot/Data/Package.json';
         $class = File::basename($url_package, $object->config('extension.json'));
         $packages = $object->data_read($url_package);
+        $node = new Node($object);
         if($packages){
-            foreach($packages->data('Package') as $nr => $package){
+            foreach($packages->data($class) as $nr => $package){
                 $options = [
                     'where' => 'name === "' . $package  . '"'
                 ];
-                $response = $object->data_record(
+                $response = $node->record(
                     'System.Installation',
                     $object->role_system(),
                     $options
