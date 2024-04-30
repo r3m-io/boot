242c56d66d8d3807b7e595c66e854894e7ffcca1
remco van der Velde <remco.pc@outlook.com>
2024-04-30 12:37:47 +0200

    trying out something

--- src/Package/Trait/Init.php
+++ src/Package/Trait/Init.php
@@ -67,7 +67,6 @@ trait Init {
                     }
                 }
                 if(property_exists($options, 'force')){
-                    Core::interactive();
                     $command = Core::binary($object) . ' install ' . $package;
                     if(!empty($command_options)){
                         $command = $command . ' ' . implode(' ', $command_options);
@@ -82,7 +81,6 @@ trait Init {
                     $is_install = true;
                 }
                 elseif(!$response){
-                    Core::interactive();
                     $command = Core::binary($object) . ' install ' . $package;
                     if(!empty($command_options)){
                         $command = $command . ' ' . implode(' ', $command_options);
